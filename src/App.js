b71bceafffcd02c3ad0c9e52b4807096729725b7
Charles Bamford <66028109+bumpusfrancus@users.noreply.github.com>
2021-12-06 13:56:05 -0800

    Add comments and update function names.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import { useState } from "react";
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 
+const HABITS = "HABITS";
+
+/**
+ * Get today's date in ISO-8601's date format.
+ */
 const today = () => {
   const date = new Date();
   const y = date.getFullYear();
@@ -10,6 +15,9 @@ const today = () => {
   return [y, m, d].join("-");
 }
 
+/**
+ * Calculate the length of consecutive successes.
+ */
 const spree = (storage, date) => {
   const sorted = [...storage.dates].sort().reverse().filter((d) => date >= d);
   let lastDate = new Date(sorted.shift());
@@ -28,26 +36,73 @@ const spree = (storage, date) => {
   return count;
 }
 
+/**
+ * Retrieve a storage object.
+ */
+const getStorage = (type) => {
+  return JSON.parse(localStorage.getItem(type))
+}
+
+/**
+ * Get the list of habits in storage.
+ */
+const getHabits = () => JSON.parse(localStorage.getItem(HABITS)) || [];
+
+/**
+ * Determine the appropriate color for a habit success.
+ */
+const color = (storage, date) => {
+  let h = Math.floor(spree(storage, date) * 12) % 360;
+  let l = 52 + Math.floor(spree(storage, date) / 30) * 4 + "%";
+  const color = `hsl(${h}, 70%, ${l})`;
+  return color;
+}
+
+/**
+ * Get all successful habit events.
+ */
+const events = getTypes().reduce(
+  (acc, type) => {
+    const storage = getStorage(type);
+    acc.push(...storage.dates.map(
+      (date) => ({
+        title: storage.name,
+        date,
+        color: color(storage, date)
+      })
+    ))
+    return acc;
+  }, []
+);
+
+/**
+ * A habit tracking app that counts streaks of successful days and displays them in a 
+ * delightful rainbow.
+ */
 export default function App() {
   const [_, setNow] = useState(Date.now());
   const rebuild = () => setNow(Date.now());
-  const getStorage = (type) => {
-    return JSON.parse(localStorage.getItem(type))
-  }
 
-  const addDoer = (type, name) => {
+  /**
+   * Create a new habit.
+   */
+  const addHabit = (type, name) => {
     if (!localStorage.getItem(type)) {
       localStorage.setItem(type, JSON.stringify({name, dates: []}));
-      if (!localStorage.getItem("types")) {
-        localStorage.setItem("types", "[]");
+      if (!localStorage.getItem(HABITS)) {
+        localStorage.setItem(HABITS, "[]");
       }
-      const types = JSON.parse(localStorage.getItem("types"));
+      const types = JSON.parse(localStorage.getItem(HABITS));
       types.push(type);
-      localStorage.setItem("types", JSON.stringify(types));
+      localStorage.setItem(HABITS, JSON.stringify(types));
     }
+    rebuild();
   }
 
-  const getDoer = (type, storage) => {
+  /**
+   * Construct a function to execute a habit.
+   */
+  const getHabitExecutor = (type, storage) => {
     return () => {
       const date = today();
       if (!storage.dates.includes(date)) {
@@ -58,34 +113,9 @@ export default function App() {
     }
   }
 
-  const getTypes = () => JSON.parse(localStorage.getItem("types")) || [];
-
-  const color = (storage, date) => {
-    let h = Math.floor(spree(storage, date) * 12) % 360;
-    let l = 52 + Math.floor(spree(storage, date) / 30) * 4 + "%";
-    const color = `hsl(${h}, 70%, ${l})`;
-    return color;
-  }
-
-  const events = getTypes().reduce(
-    (acc, type) => {
-      const storage = getStorage(type);
-      acc.push(...storage.dates.map(
-        (date) => ({
-          title: storage.name,
-          date,
-          color: color(storage, date)
-        })
-      ))
-      return acc;
-    }, []
-  );
-
-  const inputs = getTypes().map((type) => {
-    const storage = getStorage(type);
-    return <input type="button" key={type} value={storage.name} onClick={getDoer(type, storage)} />
-  });
-
+  /**
+   * Callback to add a new habit to the list.
+   */ 
   function AddHabit() {
     const [[type, name], setState] = useState(["", ""])
     const setType = (e) => {
@@ -96,11 +126,20 @@ export default function App() {
     return <div>
       <label htmlFor="addHabit">Name</label>
       <input type="text" id="addHabit" onChange={setType}/>
-      <input type="button" value="Add Habit" onClick={() => addDoer(type, name)} />
+      <input type="button" value="Add Habit" onClick={() => addHabit(type, name)} />
     </div>
   }
+
+  // Construct a list of buttons for each habit.
+  const inputs = getTypes().map((type) => {
+    const storage = getStorage(type);
+    return <input type="button" key={type} value={storage.name} onClick={getHabitExecutor(type, storage)} />
+  });
+
+  // Add the AddHabit button.
   inputs.push(<AddHabit key="addHabit"/>);
 
+  // Create the app.
   return (
     <div className="App" style={{display: "flex", flexDirection: "row"}}>
       <div style={{height: "100vh", width: "12vw", display: "flex", flexDirection: "column"}} >
